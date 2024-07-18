fbbf4ecaa4b1d7842270452fa49faea0d134cde5
anvar-muzaffarli <prof.anvar2013@gmail.com>
2024-07-18 16:10:35 +0400

    PART2 LOGIN UGURLA BITDI. PROXY QURASHDIRILDI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,28 @@
 
 import './App.css'
 import "flowbite"
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './components/auth/Login'
+
+import { Toaster } from 'react-hot-toast'
+
+import Home from './components/Home'
 
 function App() {
 
   return (
    <>
-   <Login />
+   <BrowserRouter>
+
+   <Toaster />
+   <Routes>
+
+    <Route path='/' element={<Home />} />
+    <Route path='/login' element={<Login />} />
+   </Routes>
+   </BrowserRouter>
+  
    </>
   )
 }
